3ade09b10a646241826d02957dcf54f1c5213897
badal047 <srikrishnabadal1008@gmail.com>
2024-02-26 18:08:58 +0530

    Creating SignUp API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,9 +17,12 @@ mongoose
   
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 )
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    passoword: {
+    password: {
         type: String,
         required: true,
     }
