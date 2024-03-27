2d425f890c19498685e7f6aea3b37d0d82f47927
badal047 <srikrishnabadal1008@gmail.com>
2024-03-27 17:27:05 +0530

    Create-Listing Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -179,6 +180,9 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
 
       </form>
       <div className="flex justify-between mt-5">
